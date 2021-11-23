6fbdf00dd3491b28afccc3686e07b26dd14c2fbe
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2021-11-23 16:02:02 -0500

    Conditional Mounting Example

--- src/js/tempPage.js
+++ src/js/tempPage.js
@@ -3,14 +3,25 @@ import "../css/index.css"
 import {useState, useEffect} from "react";
 
 export default function register(){
-    //fetch("http://localhost:3000/")
-    
+    const [mount, setMount] = useState(false)
+    //this is basically your component did mount, do your fetch here
+    useEffect(() => {
+        let body = "Set Body to whatever you wanted to do with it"
+        fetch('https://localhost:3000/testAPI/WHATEVERROUTEHERE', body).then((response) => {
+            console.log(response)
+            setMount(true);
+        })
+    })
         return (
-            <div>
-                <body>
-                    <h1>No peeking unless you're logged in ;)</h1>
-                </body>
-            </div>
+            <>
+                {mount ?
+                <div>
+                    <body>
+                        <h1>No peeking unless you're logged in ;)</h1>
+                    </body>
+                </div>
+                    : null}
+            </>
         );
     
 }
