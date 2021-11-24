686a0fdd372f3a8700f14a4d1deabf068fa786cf
GarreBrenn <garrebrenna@gmail.com>
2021-11-24 14:36:14 -0500

    added more UI to checkin page

--- src/js/CheckIn.js
+++ src/js/CheckIn.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { Typography } from '@mui/material';
+import { Typography, Button, Input } from '@mui/material';
 import { useLocation } from "react-router-dom";
+import "../css/CheckIn.css";
 // const QRCode = require('qrcode.react');
 // import { QrCodeScanner } from '@mui/icons-material';
-import QrReader from 'react-qr-scanner';
+//import QrReader from 'react-qr-scanner';
 
 function CheckIn() {
     const location = useLocation();
@@ -12,7 +13,12 @@ function CheckIn() {
     const [spot, setSpot] = useState({});
     const [valid, setValid] = useState(false);
     const [qrData, setQrData] = useState(null);
+    const [file, setFile] = useState(null);
     const spot2 = {}
+
+    function handleFileUpload(event) {
+        setFile(URL.createObjectURL(event.target.files[0]));
+    }
 
     function handleScan(data) {
         setQrData(data);
@@ -70,31 +76,40 @@ function CheckIn() {
     }, []);
 
     if (valid) {
-    return (
-        <div className="container">
-            <div className="title">
-                <Typography variant="h4">Check In</Typography>
-                <Typography variant="subtitle1" color="text.secondary" >
-                    {valid} {spot.Record?.Address}
-                </Typography>
-            </div>
-            <div className="body">
-                <Typography variant="h5">Scan this</Typography>
-            </div>
-            <QrReader
-                delay={100}
-                style={previewStyle}
-                onError={handleScanError}
-                onScan={handleScan}
-            />
-            <p>qrdata: {qrData}</p>
-            {/*This is the file input*/}
-            <input type="file" id="input"/>
-
-        </div>
-    )} else {
         return (
-            <Typography style={{marginTop: "50px"}} variant="h4" align="center">
+            <div className="container">
+                <div className="title">
+                    <Typography variant="h4">Check In</Typography>
+                    <Typography variant="subtitle1" color="text.secondary" >
+                        {valid} {spot.Record?.Address}
+                    </Typography>
+                </div>
+                <div className="body">
+                    <Typography variant="h5">Your upload</Typography>
+                    <Typography variant="body2" color="text.secondary" >
+                        To check in to this spot, upload an image of the QR code that the host has left.
+                        If checkin is successful, you can return to this same spot to check out.
+                    </Typography>
+                    <img className="spotImg" src={file ? file : "https://shacknews-ugc.s3.us-east-2.amazonaws.com/user/9647/article-inline/2021-03/template.jpg?versionId=EPuOpjX7pGmrwxIxaF8BBrMfaK4X7f.S"} />
+                    <Button
+                        variant="contained"
+                        component="label"
+                    >
+                        Upload File
+                        <input
+                            type="file"
+                            id="input"
+                            accept="image/png, image/jpeg"
+                            onChange={handleFileUpload}
+                            hidden
+                        />
+                    </Button>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <Typography style={{ marginTop: "50px" }} variant="h4" align="center">
                 We're having trouble processing your request. Please try again later.
             </Typography>
         )
