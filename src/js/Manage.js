240317440612c9888c5f74c84d2232b42a2231d2
GarreBrenn <garrebrenna@gmail.com>
2021-11-28 14:37:53 -0500

    implemented getting host spots

--- src/js/Manage.js
+++ src/js/Manage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import { Grid, Box } from '@mui/material'
 import SpotCard from './SpotCard.js'
+import { getUserID } from './checkCookieExists.js';
 
 function Manage() {
     const [cardData, setCardData] = useState([]);
@@ -10,26 +11,31 @@ function Manage() {
     console.log(location);
 
     useEffect(() => {
-        // TODO: REPLACE WITH /gethostspots
-        fetch('http://localhost:3000/testAPI/query', {
+        const myData = {
+            // HostID: getUserID()
+            HostID: 1
+        }
+        fetch('http://localhost:3000/testAPI/gethostspots', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
-            }
+            },
+            body: JSON.stringify(myData),
         }).then((response) =>
             response.json().then((data) => {
                 setCardData(data);
             }))
     }, []);
 
-    if (cardData !== null) {
+    console.log(cardData)
+    if (cardData.length !== 0) { 
         return (
             <Box sx={{ flexGrow: 1, marginTop: 5 }}>
                 <Grid container rowSpacing={5}>
                     {
                         cardData.map(d => {
                             return <Grid item key={d.Key} xs={12} md={6} lg={4} xl={3}>
-                                <SpotCard content={d} manage/>
+                                <SpotCard content={{Record: d}} manage/>
                             </Grid>
                         })
                     }
@@ -37,7 +43,7 @@ function Manage() {
             </Box>
         );
     } else {
-        return (<h1>Loading...</h1>);
+        return (<h1>It looks like you don't have any spots...</h1>);
     }
 }
 
