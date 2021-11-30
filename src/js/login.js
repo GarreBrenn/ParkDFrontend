5cac587e30cf35868699d9e9ed6abfb097c5a581
Funderburkhr <94197884+Funderburkhr@users.noreply.github.com>
2021-11-29 20:48:26 -0500

    Add files via upload

--- src/js/login.js
+++ src/js/login.js
@@ -1,26 +1,86 @@
-import React from "react";
-import "../css/index.css"
-export default class login extends React.Component{
-    render(){
-        return (
-            <div>
-                <body>
-                    <h1>Please login with your credentials</h1>
-                    <form action="http://localhost:3000/testAPI/login" method="POST">
-                        <label for="Email">Input email address  </label>
-                            <input type="email" id = "Email" name = "Email"/>
-                            <br></br>
-                        <label for="Password">Password  </label>
-                            <input type="password" id = "Password" name = "Password"/>
-                            <br></br>
-                     <button type="submit">Login</button>
-                     </form>
-                     <h3>Or register! It's free</h3>
-                     <a href ="/register">Register here</a>
-                     
-                        
-                </body>
-            </div>
-        );
-    }
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+        
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {/* <Avatar alt="ParkD Logo" src="/home/asd/Downloads/ParkDFrontend/src/ParkD_logo.png" /> */}
+          <Avatar >PD</Avatar>
+            
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            
+          
+          </Box>
+        </Box>
+       
+      </Container>
+     
+    </ThemeProvider>
+        
+  );
 }
